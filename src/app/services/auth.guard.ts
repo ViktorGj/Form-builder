9781bc7ddaddca9fd98e95241e6a955a61fe6251
Aleksandar Djurovski <aleksandar.djurovski@it-labs.com>
2019-05-08 14:39:21 +0200

    create model for user and shared folder

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      if (this.isAuthenticated) {
+      if (!this.isAuthenticated) {
         this.router.navigate(['login']);
         return false;
       }
