babccdda16240f96b427fa59da192fa2275d87fc
Aleksandar Atanasov <aleksandar.atanasov@it-labs.com>
2019-05-17 13:37:27 +0200

    fix

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
