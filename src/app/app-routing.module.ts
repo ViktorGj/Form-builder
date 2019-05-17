7efdd625df04863b1b763c6aaad36b7779a5253d
ViktorGj <35962456+ViktorGj@users.noreply.github.com>
2019-05-17 11:43:08 +0200

    userLayout module & router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './public/login/login.component';
 import { dashboardRoute } from './private/dashboard/dashboard-routing.module';
 import { KitchenSinkComponent } from './components/kitchen-sink/kitchen-sink.component';
+import { userLayoutRoutes } from './public/user-layout/user-layout-routing.module';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'sink', component: KitchenSinkComponent },
-  ...dashboardRoute
+  ...dashboardRoute,
+  ...userLayoutRoutes
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
 import { KitchenSinkComponent } from './components/kitchen-sink/kitchen-sink.component';
 import { DashboardModule } from './private/dashboard/dashboard.module';
 import { DragAndDropModule } from './private/drag-and-drop/drag-and-drop.module';
+import { UserLayoutModule } from './public/user-layout/user-layout.module';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { DragAndDropModule } from './private/drag-and-drop/drag-and-drop.module'
     PublicModule,
     SharedModule,
     DashboardModule,
-    DragAndDropModule
+    DragAndDropModule,
+    UserLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
