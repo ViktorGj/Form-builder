3e6a099a20be55ece7da6201c534781958dbe660
Aleksandar Atanasov <aleksandar.atanasov@it-labs.com>
2019-05-20 10:39:49 +0200

    drag drop module inport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { PublicModule } from './public/public.module';
 import { SharedModule } from './shared/shared.module';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-
-import { KitchenSinkComponent } from './components/kitchen-sink/kitchen-sink.component';
 import { DashboardModule } from './private/dashboard/dashboard.module';
 import { DragAndDropModule } from './private/drag-and-drop/drag-and-drop.module';
 import { UserLayoutModule } from './public/user-layout/user-layout.module';
 import { LoginModule } from './public/login/login.module';
 
+import { AppComponent } from './app.component';
+import { KitchenSinkComponent } from './components/kitchen-sink/kitchen-sink.component';
+
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -28,7 +27,8 @@ import { LoginModule } from './public/login/login.module';
     DashboardModule,
     DragAndDropModule,
     UserLayoutModule,
-    LoginModule
+    LoginModule,
+    DragDropModule
   ],
   exports: [
     SharedModule
