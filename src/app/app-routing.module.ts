0ef25c45f2b26699ff088cc5fe3add997e447490
Aleksandar Djurovski <aleksandar.djurovski@it-labs.com>
2019-05-20 10:40:37 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,20 +6,45 @@ import { KitchenSinkComponent } from './components/kitchen-sink/kitchen-sink.com
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { userLayoutRoutes } from './public/user-layout/user-layout-routing.module';
 import { dragRoutes } from './private/drag-and-drop/drag-and-drop-routing.module';
+import { privateRoute } from './private/private-routing.module';
+import { PrivateComponent } from './private/private.component';
+import { PublicComponent } from './public/public.component';
 
-const routes: Routes = [
-  { path: 'sink', component: KitchenSinkComponent },
-  ...dashboardRoute,
-  ...userLayoutRoutes,
+// const routes: Routes = [
+//   { path: 'sink', component: KitchenSinkComponent },
+//   // ...dashboardRoute,
+//   ...userLayoutRoutes,
+//   ...loginRoutes,
+//   // ...dragRoutes,
+//   ...privateRoute,
+//   { path: 'PageNotFound' , component: PageNotFoundComponent },
+//   { path: '**', redirectTo: 'PageNotFound'}
+// ];
+
+export const PUBLIC_ROUTES: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   ...loginRoutes,
-  ...dragRoutes,
+  ...userLayoutRoutes
+];
+
+export const SECURE_ROUTES: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  ...dashboardRoute,
+  // ...dragRoutes
+];
+
+const APP_ROUTES: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full', },
+  { path: '', component: PublicComponent, children: PUBLIC_ROUTES },
+  { path: '', component: PrivateComponent, children: SECURE_ROUTES },
   { path: 'PageNotFound' , component: PageNotFoundComponent },
   { path: '**', redirectTo: 'PageNotFound'}
 ];
 
+
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(APP_ROUTES),
 
   ],
   exports: [RouterModule]
